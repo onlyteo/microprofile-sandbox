53493764e27d04aaf353144cfa7d2e2db38342bb
Thomas <onlyteo@live.com>
2025-06-23 16:44:47 +0200

    Updated dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,11 +25,11 @@ plugins {
     kotlin("plugin.allopen") version "2.1.21" apply false
     kotlin("plugin.spring") version "2.1.21" apply false
     kotlin("plugin.serialization") version "2.1.21" apply false
-    id("org.springframework.boot") version "3.5.0" apply false
+    id("org.springframework.boot") version "3.5.3" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
     id("com.google.cloud.tools.jib") version "3.4.5" apply false
     id("org.jooq.jooq-codegen-gradle") version "3.20.4" apply false
-    id("com.expediagroup.graphql") version "8.4.0" apply false
+    id("com.expediagroup.graphql") version "8.8.1" apply false
     id("io.openliberty.tools.gradle.Liberty") version "3.8.3" apply false
 }
 
