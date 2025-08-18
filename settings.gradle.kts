9c210c4d689254f87b78b7a17930a233572f5e69
Thomas <onlyteo@live.com>
2025-08-18 15:06:43 +0200

    Updated dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,14 +21,14 @@ dependencyResolutionManagement {
 
 // PLUGINS
 plugins {
-    kotlin("jvm") version "2.2.0" apply false
-    kotlin("plugin.allopen") version "2.2.0" apply false
-    kotlin("plugin.spring") version "2.2.0" apply false
-    kotlin("plugin.serialization") version "2.2.0" apply false
+    kotlin("jvm") version "2.2.10" apply false
+    kotlin("plugin.allopen") version "2.2.10" apply false
+    kotlin("plugin.spring") version "2.2.10" apply false
+    kotlin("plugin.serialization") version "2.2.10" apply false
     id("org.springframework.boot") version "3.5.4" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
     id("com.google.cloud.tools.jib") version "3.4.5" apply false
-    id("org.jooq.jooq-codegen-gradle") version "3.20.5" apply false
+    id("org.jooq.jooq-codegen-gradle") version "3.20.6" apply false
     id("com.expediagroup.graphql") version "8.8.1" apply false
     id("io.openliberty.tools.gradle.Liberty") version "3.8.3" apply false
 }
